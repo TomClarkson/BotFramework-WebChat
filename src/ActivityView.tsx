ed3823ef653bdae78263ff50444e9dbb5daf005c
Nicolas Santini <santini.nicolas@icloud.com>
2017-06-12 08:35:27 +1200

    rendering send geolocation event

--- src/ActivityView.tsx
+++ src/ActivityView.tsx
@@ -78,6 +78,7 @@ export class ActivityView extends React.Component<ActivityViewProps, {}> {
                 );
 
             case 'typing':
+            default:
                 return <div className="wc-typing"/>;
         }
     }
