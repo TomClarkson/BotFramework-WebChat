69ec8946dbd466a40f95b0843ff2944c0e58f52a
Nicolas Santini <santini.nicolas@icloud.com>
2017-06-19 13:27:03 +1200

    adding geolocaiton middleware to store

--- src/Store.ts
+++ src/Store.ts
@@ -482,6 +482,7 @@ export const createStore = () =>
             history
         }),
         applyMiddleware(createEpicMiddleware(combineEpics(
+            sendGeolocation,
             updateSelectedActivity,
             sendMessage,
             trySendMessage,
